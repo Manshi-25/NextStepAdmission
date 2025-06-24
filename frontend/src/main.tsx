cabcfc70fecdab48b40086c840013f14a46e46f7
Manshi <09317711623_ml@vipstc.edu.in>
2025-06-24 23:36:57 +0530

    countries

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { AuthProvider } from './components/AuthContext';
-import { ProfileProvider } from './components/ProfileContext';
+//import { ProfileProvider } from './components/ProfileContext';
 createRoot(document.getElementById("root")!).render(<AuthProvider>
-                                                    <ProfileProvider>  
-                                                        <App />
-                                                    </ProfileProvider>
+                                                     <App />
                                                     </AuthProvider>);
 
 
